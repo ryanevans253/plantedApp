19cc2451261a76eb364dc4b5134aeb0b70ab28a1
Ryan <ryanevans253@yahoo.com>
2022-05-01 13:03:52 -0400

    started grid build

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import PermanentDrawerLeft from "./components/navigation/DrawerNav";
 import AllPlantCards from "./components/allPlants/AllPlants";
 import SinglePlantPage from "./pages/SinglePlant";
 
+
 function App() {
   return (
     <div className="App">
       <PermanentDrawerLeft activePage={<SinglePlantPage />} />
+
     </div>
   );
 }
--- src/pages/SinglePlant.js
+++ src/pages/SinglePlant.js
@@ -2,10 +2,33 @@
 
 import PhotoQuilt from "../components/singlePlantPage/PhotoQuilt";
 import { MyQuilt } from "../components/singlePlantPage/PhotoQuilt";
+import Grid from "@mui/material/Grid";
+import Paper from "@mui/material/Paper";
+import { styled } from "@mui/material/styles";
+import BasicTimeline from "../components/singlePlantPage/History";
+
+const Item = styled(Paper)(({ theme }) => ({
+  backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
+  ...theme.typography.body2,
+  padding: theme.spacing(1),
+  textAlign: "center",
+  color: theme.palette.text.secondary,
+}));
 
 export default function SinglePlantPage(props) {
   return (
     <div>
+      <Grid container spacing={2}>
+        <Grid item xs={12} md={6} lg={8}>
+          <Item>Left </Item>
+        </Grid>
+        <Grid item xs={12} md={6} lg={4}>
+          <Item>
+            <BasicTimeline />
+          </Item>
+        </Grid>
+      </Grid>
+
       <MyQuilt />
     </div>
   );
